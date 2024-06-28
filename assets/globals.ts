1daff81ad1594c3694c807d49e04340d4b200c15
João Oliveira <jonnasprime@gmail.com>
2024-06-28 18:18:42 +0100

    Deploy 0.1.0 version.

---
+++ assets/globals.ts
@@ -0,0 +1,16 @@
+const urls = {
+  CONTACT: 'contact',
+  EDUCATION: 'education',
+  HOME: '',
+  PROJECTS: 'projects',
+  CAREER: 'career',
+};
+
+const externalUrls = {
+  LINKEDIN: 'https://www.linkedin.com/in/joao-f-oliveira/',
+};
+
+export const GLOBALS = {
+  URLS: urls,
+  EXTERNAL_URLS: externalUrls,
+};
